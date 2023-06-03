69b3b3e00c9c57cdd3863740db5ef9b05a93d22f
benito pedro <benitopedro21@outlook.com>
2023-06-03 18:12:20 -0300

    wip: menu mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-
 export default function Home() {
   return (
     <main>
-      Home
+      <img
+        src="https://framerusercontent.com/images/ySFQvJc37OSkduwj8LSLG0cCXig.jpeg"
+        alt="hero background image"
+        style={{
+          pointerEvents: "none",
+          userSelect: "none",
+          display: "block",
+          width: "100%",
+          height: "100%",
+          borderRadius: "inherit",
+          objectPosition: "center center",
+          objectFit: "cover",
+          imageRendering: "auto",
+          minHeight: "512px",
+        }}
+        height="512"
+      />
     </main>
-  )
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,104 @@
+"use client";
+
 import logo from "@/../public/logo.svg";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
-import style from '@/style/header.module.css';
+import style from "@/style/header.module.css";
 
 const Header = () => {
-	return (
-		<header
-      className={
-        style.header
-      }
-		>
-			<div
-				className={style.headerContainer}
-			>
-				<Link href="/">
-					<div
-						className={style.logoContainer}
-					>
-						<Image src={logo} width={33} height={33} alt="AI Hunt logo" />{" "}
-						<p
-							className={style.logoText}
-						>
-							AI HUNTER™
-						</p>
-					</div>
-				</Link>
+	const [isOpen, setIsOpen] = useState(false);
 
-				<div
-					className={`menu open ${style.menuOpenIcon}`}
-				>
+	const topBarVariants = {
+		open: {
+			rotate: 45,
+			y: 7,
+		},
+		closed: {
+			rotate: 0,
+			y: 0,
+		},
+	};
+
+	const bottomBarVariants = {
+		open: {
+			rotate: -45,
+			y: -8,
+		},
+		closed: {
+			rotate: 0,
+			y: 0,
+		},
+	};
+
+  const headerContainerVariants = {
+		open: {
+			height: "200px",
+      display: 'flex',
+      transition: { 
+        type: 'spring',
+        stiffness: 80,
+        // damping: 20
+      }
+		},
+		closed: {
+      display: 'none',
+			height: "auto",
+      transition: { 
+        type: 'spring',
+        stiffness: 80,
+        // damping: 20
+      }
+		},
+	};
+
+	return (
+		<header className={style.header}>
+			<div className={style.headerContainer}>
+				<div className={style.headerFirstRow}>
+					<Link href="/">
+						<div className={style.logoContainer}>
+							<Image src={logo} width={33} height={33} alt="AI Hunt logo" />{" "}
+							<p className={style.logoText}>AI HUNTER™</p>
+						</div>
+					</Link>
+
 					<div
-						className={style.menuBar}
-            style={{top: '6px'}}
-					></div>
-					<div
-						className={style.menuBar}
-            style={{bottom: '6px'}}
-					></div>
+						className={`menu open ${style.menuOpenIcon}`}
+						onClick={() => setIsOpen(!isOpen)}
+					>
+						<motion.div
+							style={{ top: "6px" }}
+							className={style.menuBar}
+							variants={topBarVariants}
+							animate={isOpen ? "open" : "closed"}
+						></motion.div>
+						<motion.div
+							style={{ bottom: "6px" }}
+							className={style.menuBar}
+							variants={bottomBarVariants}
+							animate={isOpen ? "open" : "closed"}
+						></motion.div>
+					</div>
 				</div>
+
+				<motion.nav
+					variants={headerContainerVariants}
+					animate={isOpen ? "open" : "closed"}
+          initial={false}
+          className={style.navbar}
+				>
+					<ul className={style.navLinks}>
+            <p className={style.navtext}>CHOOSE YOUR OUTPUT:</p>
+            <li className={style.navLink}>IMAGE</li>
+            <li className={style.navLink}>TEXT</li>
+            <li className={style.navLink}>VIDEO</li>
+            <li className={style.navLink}>CODE</li>
+            <li className={style.navLink}>AUDIO</li>
+          </ul>
+				</motion.nav>
 			</div>
 		</header>
 	);
