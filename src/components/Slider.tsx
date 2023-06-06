584e39fd0c7511ba5ce1afc86e2fc7de98d7dfb8
benito pedro <benitopedro21@outlook.com>
2023-06-06 19:08:54 -0300

    wip: ticker

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,16 +4,15 @@ import Ticker from 'framer-motion-ticker';
 function App() {
   const colors = ['#632bf3', '#f122c8', '#f16022', '#9ef344', '#44d3f3'];
   return (
-    <div className="App" style={{position: 'absolute',
-    bottom: '-55px'}}>
+    <div className="App" style={{position: 'absolute', bottom: '0px'}}>
       <Ticker duration={20}>
         {colors.map((item, index) => (
           <div
             key={index}
             style={{
               backgroundColor: item,
-              margin: '5px',
-              height: '250px',
+              margin: '10px',
+              height: '90px',
               width: '200px',
             }}
           />
